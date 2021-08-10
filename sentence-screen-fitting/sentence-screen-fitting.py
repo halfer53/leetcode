53ca1477fc22bd2bdfb7d5e06d6897cb7a32204c
Bruce <brucetansh@gmail.com>
2021-08-10 21:19:07 +1200

    Time: 152 ms (71.05%), Space: 14.4 MB (35.23%) - LeetHub

--- sentence-screen-fitting/sentence-screen-fitting.py
+++ sentence-screen-fitting/sentence-screen-fitting.py
@@ -5,10 +5,10 @@ class Solution:
         start = 0
         for i in range(rows):
             start += cols
-            if s[(start % length)] == ' ':
+            if s[(start) % length] == ' ':
                 start += 1
             else:
-                while start and s[((start - 1) % length)]  != ' ':
+                while start >= 0 and s[(start - 1) % length] != ' ':
                     start -= 1
         return start // length
         
