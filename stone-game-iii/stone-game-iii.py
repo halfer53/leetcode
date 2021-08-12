074bd615bb73002537377957166849e24f459337
Bruce <brucetansh@gmail.com>
2021-08-12 13:04:37 +1200

    Time: 5268 ms (11.83%), Space: 505.9 MB (5.08%) - LeetHub

--- stone-game-iii/stone-game-iii.py
+++ stone-game-iii/stone-game-iii.py
@@ -5,30 +5,22 @@ class Solution:
         def dfs(i: int, turn: bool) -> int:
             if i >= n:
                 return 0
-            
             if turn:
-                v1 = r1 = v2 = r2 = v3 = r3 = -float('inf')
-                v1 = stoneValue[i]
-                r1 = dfs(i + 1, not turn)
-                if n - i >= 2:
-                    v2 = sum(stoneValue[i:i+2])
-                    r2 = dfs(i + 2, not turn)
-                if n - i >= 3:
-                    v3 = sum(stoneValue[i:i+3])
-                    r3 =  dfs(i + 3, not turn)
-                return max(v1 + r1, v2 + r2, v3 + r3)
+                r1 = r2 = r3 = -float('inf')
+                r1 = dfs(i+1, not turn) + stoneValue[i]
+                if i < n - 1:
+                    r2 = dfs(i + 2, not turn) + sum(stoneValue[i:i+2])
+                if i < n - 2:
+                    r3 = dfs(i + 3, not turn) + sum(stoneValue[i:i+3])
+                return max(r1, r2, r3)
             else:
-                v1 = r1 = v2 = r2 = v3 = r3 = -float('inf')
-                v1 = stoneValue[i]
-                r1 = dfs(i + 1, not turn)
-                if n - i >= 2:
-                    v2 = sum(stoneValue[i:i+2])
-                    r2 = dfs(i + 2, not turn)
-                if n - i >= 3:
-                    v3 = sum(stoneValue[i:i+3])
-                    r3 =  dfs(i + 3, not turn)
-                return min(-v1 + r1, -v2 + r2, -v3 + r3)
-            
+                r1 = r2 = r3 = float('inf')
+                r1 = dfs(i + 1, not turn) - stoneValue[i]
+                if i < n - 1:
+                    r2 = dfs(i + 2, not turn) - sum(stoneValue[i:i+2])
+                if i < n - 2:
+                    r3 = dfs(i + 3, not turn) - sum(stoneValue[i:i+3])
+                return min(r1, r2, r3)
         ret = dfs(0, True)
         if ret > 0:
             return 'Alice'
