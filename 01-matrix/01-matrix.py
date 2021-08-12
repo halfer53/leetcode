e6e6b308b439d935a54271e4209a1e17a587683a
Bruce <brucetansh@gmail.com>
2021-08-12 12:03:01 +1200

    Time: 572 ms (85.62%), Space: 17 MB (75.83%) - LeetHub

--- 01-matrix/01-matrix.py
+++ 01-matrix/01-matrix.py
@@ -2,20 +2,21 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         m = len(mat)
         n = len(mat[0])
-        ret = [[float('inf')] * n for _ in range(m)]
+        dp = [[float('inf')] * n for _ in range(m)]
         for i in range(m):
             for j in range(n):
                 if mat[i][j]:
-                    if j:
-                        ret[i][j] = min(ret[i][j], ret[i][j-1] + 1)
                     if i:
-                        ret[i][j] = min(ret[i][j], ret[i-1][j] + 1)
+                        dp[i][j] = min(dp[i][j], dp[i-1][j] + 1)
+                    if j:
+                        dp[i][j] = min(dp[i][j], dp[i][j-1] + 1)
                 else:
-                    ret[i][j] = mat[i][j]
-        for i in reversed(range( m)):
-            for j in reversed(range( n)):
-                if j < n - 1:
-                    ret[i][j] = min(ret[i][j], ret[i][j+1] + 1)
-                if i < m - 1:
-                    ret[i][j] = min(ret[i][j], ret[i+1][j] + 1)
-        return ret
+                    dp[i][j] = mat[i][j]
+        for i in reversed(range(m)):
+            for j in reversed(range(n)):
+                if mat[i][j]:
+                    if i < m -1:
+                        dp[i][j] = min(dp[i][j], dp[i+1][j] + 1)
+                    if j < n - 1:
+                        dp[i][j] = min(dp[i][j], dp[i][j+1] + 1)
+        return dp
