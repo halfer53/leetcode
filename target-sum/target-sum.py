2baf34a0ce2ab0875906ffc3ea0cfc506b9cc441
Bruce <brucetansh@gmail.com>
2021-10-01 00:37:02 +1300

    Time: 276 ms (66.28%), Space: 42.4 MB (5.14%) - LeetHub

--- target-sum/target-sum.py
+++ target-sum/target-sum.py
@@ -1,21 +1,15 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        self.target = target
-        self.nums = nums
-        self.n = len(nums)
-        self.mem = dict()
-        return self.backtrack(0, 0)
+        n = len(nums)
         
-    def backtrack(self, i: int, curr: int):
-        if i >= self.n:
-            if curr == self.target:
+        @cache
+        def dfs(i: int, curr: int) -> int:
+            if i >= n and curr == target:
                 return 1
-            return 0
-        if (i, curr) in self.mem:
-            return self.mem[(i, curr)]
-        num = self.nums[i]
-        r1 = self.backtrack(i + 1, curr - num)
-        r2 = self.backtrack(i + 1, curr + num)
-        ret = r1 + r2
-        self.mem[(i,curr)] = ret
-        return ret
+            if i >= n:
+                return 0
+            positive = dfs(i+1, curr + nums[i])
+            negative = dfs(i+1, curr - nums[i])
+            return positive + negative
+        return dfs(0, 0)
+        
