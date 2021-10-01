3cf242f7690f100e35bcd42b63a558d644c0969a
Bruce <brucetansh@gmail.com>
2021-10-01 22:31:00 +1300

    Time: 137 ms (92.35%), Space: 14.3 MB (87.92%) - LeetHub

--- target-sum/target-sum.py
+++ target-sum/target-sum.py
@@ -1,15 +1,19 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        tsum = sum(nums)
         n = len(nums)
+        if (tsum + target) % 2 != 0 or tsum < abs(target):
+            return 0
+        s1 = (tsum + target) // 2
+        dp = [[0] * (s1+1) for _ in range(n+1)]
+        for i in range(n+1):
+            dp[i][0] = 1
+        for i in range(1, n+1):
+            for j in range( s1+1):
+                if j < nums[i-1]:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j - nums[i-1]] + dp[i-1][j]
+        return dp[n][s1]
         
-        @cache
-        def dfs(i: int, curr: int) -> int:
-            if i >= n and curr == target:
-                return 1
-            if i >= n:
-                return 0
-            positive = dfs(i+1, curr + nums[i])
-            negative = dfs(i+1, curr - nums[i])
-            return positive + negative
-        return dfs(0, 0)
         
