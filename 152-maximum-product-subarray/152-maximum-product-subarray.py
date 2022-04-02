9ff41b8440e35af33988a3c8d39b80ad947e938e
Bruce <brucetansh@gmail.com>
2022-04-02 23:00:54 +1300

    Time: 124 ms (45.54%), Space: 14.3 MB (82.94%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,21 +1,12 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        def get_max(numbers):
-            largest_prod = -math.inf
-            current_prod = 1
-        
-            for num in numbers:
-                current_prod *= num
-                largest_prod = max(current_prod, largest_prod)
-                
-                # Reset if 0 is seen
-                if current_prod == 0: current_prod = 1
-            
-            return largest_prod
-        
-        
-        return max(
-            get_max(nums), 
-            get_max(reversed(nums))
-        )
-            
+        neg = nums[0]
+        pos = nums[0]
+        ret = nums[0]
+        for val in nums[1:]:
+            tmppos = val * pos
+            tmpneg = val * neg
+            pos = max(tmppos, val, tmpneg)
+            neg = min(tmppos, val, tmpneg)
+            ret = max(ret, pos)
+        return ret 
