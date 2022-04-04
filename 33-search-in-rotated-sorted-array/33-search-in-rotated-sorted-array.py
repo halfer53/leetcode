8ff55c76ae8d5e20f243101f5d949581ca00e8ec
Bruce <brucetansh@gmail.com>
2022-04-04 23:50:32 +1200

    Time: 57 ms (55.24%), Space: 14.2 MB (92.80%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -3,33 +3,20 @@ class Solution:
         n = len(nums)
         left = 0
         right = n - 1
-        point = 0
-        if nums[0] < nums[-1]:
-            ret = bisect.bisect_left(nums, target)
-            if ret != n and nums[ret] == target:
-                return ret
-            return -1
         while left <= right:
-            mid = (left + right) >> 1
-            if mid and nums[mid-1] > nums[mid]:
-                point = mid
-                break
-            elif mid < n - 1 and nums[mid] > nums[mid+1]:
-                point = mid+1
-                break
-            elif nums[0] < nums[mid]:
-                left = mid + 1
+            mid = (left + right ) >> 1
+            if nums[mid] == target:
+                return mid
+            if nums[mid] >= nums[0]:
+                if nums[0] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
             else:
-                right = mid - 1
-        print(point, nums[point:])
-        ret = 0
-        if nums[0] <= target <= nums[point-1]:
-            ret = bisect.bisect_left(nums[:point], target)
-        else:
-            ret = bisect.bisect_left(nums[point:], target)
-            ret += point
-        if 0 <= ret < n and nums[ret] == target:
-            return ret
+                if nums[mid] <= target <= nums[-1]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
         return -1
         
                 
